a6023aac2e7a03f452b0bf4f695689c0f6118eba
Bobby Martin <bobbymartin2@gmail.com>
2019-03-01 16:48:33 -0800

    add script to create minimal hyperscan nupkg

---
+++ ports/hyperscan/make-nupkg.ps1
@@ -0,0 +1,16 @@
+md custom-nupkg\installed\x64-windows-static\debug\lib
+md custom-nupkg\installed\x64-windows-static\lib
+md custom-nupkg\installed\x64-windows-static\include\hs
+md custom-nupkg\_rels
+
+cp buildtrees\hyperscan\x64-windows-static-dbg\lib\hs.lib custom-nupkg\installed\x64-windows-static\debug\lib
+cp buildtrees\hyperscan\x64-windows-static-rel\lib\hs.lib custom-nupkg\installed\x64-windows-static\lib
+cp buildtrees\hyperscan\src\*\src\*.h custom-nupkg\installed\x64-windows-static\include\hs
+cp ports\hyperscan\'``[Content_Types``].xml' custom-nupkg
+cp ports\hyperscan\_rels\.rels custom-nupkg\_rels
+cp ports\hyperscan\hyperscan-5.1.0.nuspec custom-nupkg
+
+cd custom-nupkg
+Compress-Archive -Path * -DestinationPath ..\hyperscan-5.1.0.zip
+cd ..
+move hyperscan-5.1.0.zip hyperscan-5.1.0.nupkg
