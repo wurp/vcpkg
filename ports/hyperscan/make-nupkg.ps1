911743888d0cd1066f1bba19f33b14de4b1862d6
Bobby Martin <bobbymartin2@gmail.com>
2019-05-22 14:21:28 -0700

    use canonical nupkg structure and add imports for C# and C++ projects

--- ports/hyperscan/make-nupkg.ps1
+++ ports/hyperscan/make-nupkg.ps1
@@ -12,25 +12,24 @@ echo "Deleted hyperscan dbg CMakeFiles"
 rd -Recurse buildtrees\hyperscan\x64-windows-dbg\unit
 echo "Deleted hyperscan dbg unit"
 
-md custom-nupkg\installed\x64-windows\debug\lib
-md custom-nupkg\installed\x64-windows\lib
-md custom-nupkg\installed\x64-windows\debug\bin
-md custom-nupkg\installed\x64-windows\bin
-md custom-nupkg\installed\x64-windows\include\hs
+md custom-nupkg\build
+md custom-nupkg\lib\native
+md custom-nupkg\include\hs
 md custom-nupkg\_rels
 
 echo copying hs.lib
-cp buildtrees\hyperscan\x64-windows-dbg\bin\hs.dll custom-nupkg\installed\x64-windows\debug\bin
-cp buildtrees\hyperscan\x64-windows-rel\bin\hs.dll custom-nupkg\installed\x64-windows\bin
-cp buildtrees\hyperscan\x64-windows-dbg\lib\hs.lib custom-nupkg\installed\x64-windows\debug\lib
-cp buildtrees\hyperscan\x64-windows-rel\lib\hs.lib custom-nupkg\installed\x64-windows\lib
-cp buildtrees\hyperscan\src\*\src\*.h custom-nupkg\installed\x64-windows\include\hs
+cp buildtrees\hyperscan\x64-windows-dbg\bin\hs.dll custom-nupkg\lib\native\hs-mt-gd-x64.dll
+cp buildtrees\hyperscan\x64-windows-rel\bin\hs.dll custom-nupkg\lib\native\hs-mt-x64.dll
+cp buildtrees\hyperscan\x64-windows-dbg\lib\hs.lib custom-nupkg\lib\native\hs-mt-gd-x64.lib
+cp buildtrees\hyperscan\x64-windows-rel\lib\hs.lib custom-nupkg\lib\native\hs-mt-x64.lib
+cp buildtrees\hyperscan\src\*\src\*.h custom-nupkg\include\hs
 cp ports\hyperscan\'``[Content_Types``].xml' custom-nupkg
 cp ports\hyperscan\_rels\.rels custom-nupkg\_rels
+cp ports\hyperscan\hyperscan-5.1.1.targets custom-nupkg\build
 cp ports\hyperscan\hyperscan-5.1.1.nuspec custom-nupkg
 
 cd custom-nupkg
 Compress-Archive -Path * -DestinationPath ..\hyperscan-5.1.1.zip
 cd ..
 sleep 60
-move hyperscan-5.1.1.zip hyperscan_5_1_1.1.0.3.nupkg
+move hyperscan-5.1.1.zip hyperscan_5_1_1.1.0.4.nupkg
